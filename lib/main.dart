13ae32ba61a3110eb1d4163a6ffa4d8a693b820e
kisbi3 <kisbi3@naver.com>
2022-10-16 23:57:18 +0900

    Lecture 3 Box, Alignment
    Alignment 연습

--- lib/main.dart
+++ lib/main.dart
@@ -16,19 +16,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar( title: Text('앱임'),),
-        body: Text('안녕'),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height: 60,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page),
-              ],
-            ),
+        body: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            width: 50, height: 50, color: Colors.blue,
+            margin: EdgeInsets.all(20 ),
           ),
         ),
       )
