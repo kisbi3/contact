1c59e1f3d356b2e40041b5f26482f32c3e77eb15
kisbi3 <kisbi3@naver.com>
2022-10-18 23:18:24 +0900

    Lecture 4 HomeWork(3)
    살짝 수정. 별로 차이 없음.

--- lib/main.dart
+++ lib/main.dart
@@ -30,68 +30,70 @@ class MyApp extends StatelessWidget {
             Icon(Icons.search, color: Colors.black), Icon(Icons.menu, color: Colors.black), Icon(Icons.star, color: Colors.black),
           ],
         ),
-        body: Align(
-          alignment: Alignment.topLeft,
-          child: SizedBox(
-            width: double.infinity,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: (Row(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.asset(
-                      'COVER1.jpg',
-                      width: 150,
-                      height: 150,
-                  ),
-                  Container(
-                    width: 330,
-                    padding: EdgeInsets.fromLTRB(10,0,50,0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Flexible(
-                            // child: Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)'),
-                              child: RichText(
-                            overflow: TextOverflow.clip,
-                            maxLines: 5,
-                            text: TextSpan(
-                              text: '캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)',
-                              style: TextStyle(
-                                fontSize: 25,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.fromLTRB(0,5,0,5),
-                          child: Text('성동구 행담동 · 끌올 10분 전',
+        body: Container(
+          width: double.infinity,
+          height: 150,
+          padding: EdgeInsets.all(10),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flexible(
+                flex: 3,
+                child: Image.asset(
+                    'COVER1.jpg',
+                    width: 150,
+                    height: 150,
+                ),
+              ),
+              Flexible(
+                flex: 7,
+                child: Container(
+                  width: 330,
+                  padding: EdgeInsets.fromLTRB(10,0,50,0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Flexible(
+                          // child: Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)'),
+                            child: RichText(
+                          overflow: TextOverflow.clip,
+                          maxLines: 5,
+                          text: TextSpan(
+                            text: '캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)',
                             style: TextStyle(
-                              color: Colors.grey,
+                              fontSize: 20,
                             ),
                           ),
                         ),
-                        Text('210,000원',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Icon(Icons.favorite_border, color: Colors.grey,),
-                            Text('4', style: TextStyle(color: Colors.grey),),
-                          ],
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(0,5,0,5),
+                        child: Text('성동구 행담동 · 끌올 10분 전',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
                         ),
-                      ],
-                    ),
+                      ),
+                      Text('210,000원',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Icon(Icons.favorite_border, color: Colors.grey,),
+                          Text('4', style: TextStyle(color: Colors.grey),),
+                        ],
+                      ),
+                    ],
                   ),
-                ],
-              )),
-            ),
+                ),
+              ),
+            ],
           ),
-        )
+        ),
       )
     );
   }
