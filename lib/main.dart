d104f92c88318b797cc68ea3aef6cb73417f947e
Jaesung Kim <kisbi3@naver.com>
2022-10-20 17:26:40 +0900

    End!
    Game Character information End

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,149 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-//MyApp은 stateless Widget이므로, stl+tab 하고 이름에 MyApp 넣기!
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // 앱을 통칭하는 이름(예를 들어 최근 앱 보기에 나타나는 이름)
-      title: 'First app',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      home: MyCard(),
+      //오른쪽 위에 있는 debug 표시 없애기
+      debugShowCheckedModeBanner: false,
+      title: 'BBANTO',
+      home: Grade(),
     );
   }
 }
 
-class MyCard extends StatelessWidget {
-  const MyCard({Key? key}) : super(key: key);
+class Grade extends StatelessWidget {
+  const Grade({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: Text('BBANTO'),
+        title: Text("BBANTO"),
+        backgroundColor: Colors.amber[700],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // 그림 넣기
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/practice_gif.gif'),
+                radius: 60.0,
+              ),
+            ),
+            //구분선 넣기
+            Divider(
+              // height : Divider 위의 간격과 아래의 간격의 합을 말함
+              height: 60.0,
+              color: Colors.grey[850],
+              thickness: 0.5,
+              // divider가 멈추는 지점
+              endIndent: 30.0,
+            ),
+            Text('NAME',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                )),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'BBANTO',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text('BBANTO POWER LEVEL',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                )),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '14',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'using lightsaber',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'face hero tattoo',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'fire flames',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            ),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/COVER1.jpg'),
+                radius: 40.0,
+                //사진의 배경이 투명일 경우 배경 색깔을 조정하는 것.
+                backgroundColor: Colors.amber[800],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
