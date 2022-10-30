51d2fb27688c6ddcaaa3ae7aca5c5bc750d2c634
kisbi3 <kisbi3@naver.com>
2022-10-31 00:01:44 +0900

    Toast message
    순한맛 19

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Appbar',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blue,
       ),
       home: MyPage(),
     );
@@ -24,38 +25,30 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Snack Bar'),
+        title: Text('Toast message'),
         centerTitle: true,
-        elevation: 0.0,
       ),
       body: Center(
-        // FlatButton 없음!!
-        //ElevatedButton, Raised button, Floating action button : 기능상에서는 별 차이가 없음
-        // 중앙의 버튼을 누르면 SnackBar 출력!!
         child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.red,
-          ),
-          onPressed: (){
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text('New Snack Bar',
-              textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              backgroundColor: Colors.teal,
-              duration: Duration(milliseconds: 1000),
+            onPressed: (){
+              flutterToast();
+            },
+            child: Text('Toast'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
             ),
-            );
-          },
-          child: Text('show me',
-            style: TextStyle(
-              color: Colors.white
-            ),
-          ),
         ),
       ),
     );
   }
+}
+
+void flutterToast(){
+  Fluttertoast.showToast(msg: 'Flutter',
+  gravity: ToastGravity.BOTTOM,
+  backgroundColor: Colors.redAccent,
+  fontSize: 20.0,
+  textColor: Colors.white,
+  toastLength: Toast.LENGTH_SHORT,
+  );
 }
