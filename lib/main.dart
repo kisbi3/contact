9838fe53ccea7a8d90e07cb2c6aecd16da47c115
kisbi3 <kisbi3@naver.com>
2022-11-08 01:20:46 +0900

    Container Widget 되짚어보기
    순한맛 20

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
@@ -24,31 +23,23 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Toast message'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: (){
-              flutterToast();
-            },
-            child: Text('Toast'),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue,
-            ),
+      backgroundColor: Colors.blue,
+      // SafeArea : 표현하려는 것이 화면 밖으로 나가는 것을 방지하는 역할.
+      body: SafeArea(
+        // Container는 최대한 큰 공간을 차지하려고 함!(child가 없는 경우.)
+        child: Container(
+          color: Colors.red,
+          width: 100,
+          height: 100,
+          margin: EdgeInsets.symmetric(
+            vertical: 80,
+            horizontal: 20,
+          ),
+          padding: EdgeInsets.all(20),
+          // child가 있는 경우에는 child 크기로 Container가 작아짐!
+          child: Text("Hello"),
         ),
       ),
     );
   }
-}
-
-void flutterToast(){
-  Fluttertoast.showToast(msg: 'Flutter',
-  gravity: ToastGravity.BOTTOM,
-  backgroundColor: Colors.redAccent,
-  fontSize: 20.0,
-  textColor: Colors.white,
-  toastLength: Toast.LENGTH_SHORT,
-  );
 }
