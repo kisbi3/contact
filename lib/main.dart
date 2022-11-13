18f35e04366d9a11f00a417c0203b032e8531500
kisbi3 <kisbi3@naver.com>
2022-11-14 00:22:13 +0900

    String interpolation Collection / Generic
    순한맛 24

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,30 @@
-import "package:contact2/ScreenA.dart";
-import 'package:contact2/ScreenB.dart';
-import 'package:contact2/ScreenC.dart';
-import 'package:flutter/material.dart';
+// 코딩셰프에서 DartPad에서 진행하므로, 여기에서도 그냥 DartPad처럼 진행할 것임.
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// String Interpolation
+import 'package:flutter/cupertino.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    // Multipage 구현 할 때에 핵심적인 것 :
-    // initialRoute : 제일 처음에 보일 화면 (home argument 대신 사용)
-    // routes :
-    return MaterialApp(
-      // initialRoute 는 무조건 '/'. 아래와 같이 쓰면 됨.
-      initialRoute: '/',
-      // 그리고 다른 페이지들을 'String'으로 dictionary 형태로 만들어 준다고 생각하자.
-      routes: {
-        // initialRoute가 ScreenA라는 것.
-        '/' : (context) => ScreenA(),
-        '/b' : (context) => ScreenB(),
-        '/c' : (context) => ScreenC(),
-      },
-    );
-  }
+void main(){
+  String name = "Sean";
+  print(name);
+
+  // 변수 name 불러오기 : $name
+  print("Hi, $name, what's up?");
 }
+
+//Collection, Generic
+//Collection : 데이터들을 모아서 가지고 있는 자료구조
+//Generic : Collection이 가지고 있는 데이터들의 데이터 타입을 지정해서 코드의 안정성/재사용성을 높이는 기법.
+
+void collection(){
+  // list 만드는 법
+  // 변수 선언은 List로 하면 list로 고정, var로 하면 자동으로 list로 맞춰줌
+  //1. growable(동적 배열)
+  List number1 = new List.empty(growable: true);
+  //2. fixed length(정적 배열)
+  var number2 = [];
+
+  // <> : generic -> list안에 들어가는 타입을 지정해줌
+  List<String> number3 = new List.empty(growable: true);
+}
+
