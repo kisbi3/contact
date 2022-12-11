81b055928978ff282ba7c04ac48525524c277cb2
kisbi3 <kisbi3@naver.com>
2022-12-12 01:45:40 +0900

    - flutter2.0 - Text button Elevated button Outlined button ButtonBar
    순한맛 27
    -순한맛 끝!!!-

--- lib/main.dart
+++ lib/main.dart
@@ -13,119 +13,151 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
-      home: MyPage(),
+      home: MyButtons(),
     );
   }
 }
 
-class MyPage extends StatelessWidget {
-  const MyPage({Key? key}) : super(key: key);
+
+class MyButtons extends StatelessWidget {
+  const MyButtons({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Scaffold Messenger"),
-      ),
-      body: HomeBody(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.thumb_up),
-        onPressed: (){
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text('Like a new Snack bar!'),
-            duration: Duration(seconds: 5),
-            action: SnackBarAction(label: 'Undo', onPressed: (){
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => ThirdPage()),
-              );
-            }),
-          ));
-        },
-      ),
-    );
-  }
-}
-
-class HomeBody extends StatelessWidget {
-  const HomeBody({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-          onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=> SecondPage()),);
-          },
-          child: Text('Go to the Second Page')
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  const SecondPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Second Page'),
+        title: Text('Buttons'),
+        centerTitle: true,
       ),
       body: Center(
-        child: Text(
-            '"좋아요"가 추가 되었습니다.',
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          //  그냥 TextButton
+            TextButton(
+              onPressed: (){
+                print("Text Button");
+              },
+              onLongPress: (){
+                print("Text Button2");
+              },
+              child: Text(
+                "Text button",
                 style: TextStyle(
-            fontSize: 20.0,
-            color: Colors.redAccent,
-        ),
-        ),
-      ),
-    );
-  }
-}
-
-// Snackbar가 다른 페이지로 갔을 때 바로 사라지게 하려면...
-// 개별적인 ScaffoldMessenger를 생성해야 함!
-//      => Scaffold를 ScaffoldMessenger로 감싸기고 Builder 함수를 이용해야 함.
-class ThirdPage extends StatelessWidget {
-  const ThirdPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ScaffoldMessenger(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Third Page'),
-        ),
-        body: Builder(
-          builder: (context) {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    '"좋아요"를 취소 하시겠습니까?',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.redAccent,
-                    ),
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text('"좋아요"가 취소되었습니다.'),
-                              duration: Duration(seconds: 3),
-                            )
-                        );
-                      },
-                      child: Text("취소하기"))
-                ],
+                  fontSize: 20.0
+                ),
               ),
-              //    Center 위젯은 return된 위젯이므로 ; 붙여야 함.
-            );
-          }
-        ),
-      ),
+              style: TextButton.styleFrom(
+                // 영상에서는 primary를 사용하였지만, 이제는 사용하지 않는다고 나옴.
+                // 대신 foregroundColor 사용!
+                foregroundColor: Colors.red,
+              // backgroundColor: Colors.blue
+              ),
+            ),
+            // 어느정도 떠서 보이는 버튼(그림자는 elevation으로 설정 가능)
+            ElevatedButton(
+              onPressed: (){
+                print("Elevated button");
+              },
+                child: Text('Elevated button'),
+            style:ElevatedButton.styleFrom(
+              backgroundColor: Colors.orangeAccent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10)
+              ),
+              elevation: 0.0
+            ),
+            ),
+
+            // 외곽 선이 있는 버튼
+            OutlinedButton(
+                onPressed: (){
+                  print('Outlined button');
+                },
+                child: Text('Outlined button'),
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.green,
+                // 외곽 선 설정
+                side: BorderSide(
+                  color: Colors.black87,
+                  width: 2.0
+                )
+              ),
+            ),
+
+          //  버튼 앞에 아이콘을 넣어야 하는 경우 : .icon
+            TextButton.icon(
+                onPressed: (){
+                  print('Icon button');
+                },
+                icon: Icon(
+                    Icons.home,
+                  size: 30.0,
+                  color: Colors.black87,
+                ),
+                label: Text('Go to home'),
+              style: TextButton.styleFrom(
+                // icon에서 색을 지정하지 않았더라면 여기서 지정하는 색과 동일한 색으로 설정함
+                foregroundColor: Colors.purple
+              ),
+            ),
+            ElevatedButton.icon(
+                onPressed: (){
+                  print('Go to home');
+                },
+                icon: Icon(
+                  Icons.home,
+                  size: 20.0,
+                ),
+                label: Text('Go to home'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                // 크기 설정
+                minimumSize: Size(200, 50)
+              ),
+            ),
+            OutlinedButton.icon(
+                onPressed: (){
+                  print('Go to home');
+                },
+                icon: Icon(Icons.home),
+                label: Text('Go to home'),
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.black,
+              )
+            ),
+            ElevatedButton.icon(
+              //비활성화된 버튼
+              onPressed: null,
+              icon: Icon(
+                Icons.home,
+                size: 20.0,
+              ),
+              label: Text('Go to home'),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  disabledBackgroundColor: Colors.pink,
+              ),
+            ),
+          //  context 아래에서 오른쪽 끝에 두개의 버튼을 가로정렬 시켜주는 ButtonBar
+          //  만약 가로 공간이 부족하다면 세로로 배치 해줌
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              buttonPadding: EdgeInsets.all(20),
+              children: [
+                TextButton(
+                    onPressed: (){},
+                    child: Text("TextButton"),
+                ),
+                ElevatedButton(
+                    onPressed: (){},
+                    child: Text("ElevatedButton"),
+                )
+              ],
+            )
+          ],
+        )
+      )
     );
   }
 }
